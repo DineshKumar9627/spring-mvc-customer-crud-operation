35fd16035f9ce7cbb47ea71362b48f130c0ddc37
Dinesh Kumar <dineshzz600@outlook.com>
2022-02-16 15:52:00 +0530

    Dinesh

---
+++ src/main/java/com/sunglowsys/SpringMvcCustomerCrudOperationApplication.java
@@ -0,0 +1,13 @@
+package com.sunglowsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCustomerCrudOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcCustomerCrudOperationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sunglowsys/service/CustomerServiceImpl.java
@@ -0,0 +1,58 @@
+package com.sunglowsys.service;
+
+import com.sunglowsys.domain.Customer;
+import com.sunglowsys.repository.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerServiceImpl  implements CustomerService{
+    private final Logger log = LoggerFactory.getLogger(CustomerServiceImpl.class);
+
+    private  final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+
+    @Override
+    public Customer save(Customer customer) {
+
+        log.debug("Request to create customer : {}",customer);
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+
+        log.debug("Request to update customer : {}",customer);
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public Page<Customer> findAll(Pageable pageable) {
+
+        log.debug("Request to find all customers: {}",pageable);
+        return customerRepository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<Customer> findById(Long id) {
+        log.debug("request to find customer by id : {}",id);
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        log.debug("request to delete customer : {}",id);
+        customerRepository.deleteById(id);
+
+
+    }
+}
